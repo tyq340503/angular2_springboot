65a033a91b5c41623c031e78f97e546e74e1b5b0
unknown <1135409448@qq.com>
2018-08-10 18:55:37 -0400

    change webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,62 +1,24 @@
 
-// var webpack = require('webpack');
-// var path = require('path');
-// var HtmlwebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//     entry: "./src/main,ts",
-//     output: {
-//         //制定打包后的输出文件名,这个文件需要被引入到index.html中
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: "./bundle.js"
-//     },
-//     resolve: {
-//         root: [path.join(__dirname, 'src')],
-//         extensions: ['', '.ts', '.js']
-//     },
-//     module: {
-        
-//         loader: [
-//             { test: /\.ts$/, loader: 'ts-loader' }
-//         ]
-//     },
-//     plugins: [
-//         // plugins
-//         new HtmlwebpackPlugin({
-//             template: path.resolve(__dirname, 'src/index.html'),
-//             inject: 'body'
-//         })
-//     ],
-// };
-
 var webpack = require('webpack');
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
-var config = {
-    // 入口
-    entry:  ['babel-polyfill', './src/main.ts'],
-    // 生成的文件
+
+module.exports = {
+    entry: "./src/main.ts",
     output: {
+        //制定打包后的输出文件名,这个文件需要被引入到index.html中
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: "./bundle.js"
     },
-    // 文件后缀
     resolve: {
+        root: [path.join(__dirname, 'src')],
         extensions: ['', '.ts', '.js']
     },
     module: {
-        // babel
-        rules: [
-            {
-            test: /\.ts$/,
-            // include: [/src.main/],
-            use: ["babel-loader", "awesome-typescript-loader?silent=true" , "ts-loader" , "angular2-template-loader"],
-            exclude: /node_modules/
-        },
-        {
-            test: /\.css$/, // Only .css files
-            use: ["style-loader", "css-loader"]
-        }]
+
+        loaders: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
     },
     plugins: [
         // plugins
@@ -65,6 +27,48 @@ var config = {
             inject: 'body'
         })
     ],
-    devtool: 'source-map'
 };
-module.exports = config;
+
+// var webpack = require('webpack');
+// var path = require('path');
+// var HtmlwebpackPlugin = require('html-webpack-plugin');
+// var config = {
+//     // 入口
+//     entry: ['babel-polyfill', './src/main.ts'],
+//     // 生成的文件
+//     output: {
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: 'bundle.js'
+//     },
+//     // 文件后缀
+//     resolve: {
+//         extensions: ['', '.ts', '.js']
+//     },
+//     module: {
+//         // babel
+//         // loader: [
+//         //     { test: /\.ts$/, loader: 'ts-loader' }
+//         // ],
+//         rules: [
+//             {
+//                 test: /\.ts$/,
+//                 // include: [/src.main/],
+//                 loaders: 'awesome-typescript-loader',
+//                 // use: ["babel-loader", "awesome-typescript-loader?silent=true" , "ts-loader" , "angular2-template-loader"],
+//                 exclude: /node_modules/
+//             },
+//             {
+//                 test: /\.css$/, // Only .css files
+//                 use: ["style-loader", "css-loader"]
+//             }]
+//     },
+//     plugins: [
+//         // plugins
+//         new HtmlwebpackPlugin({
+//             template: path.resolve(__dirname, 'src/index.html'),
+//             inject: 'body'
+//         })
+//     ],
+//     devtool: 'source-map'
+// };
+// module.exports = config;
