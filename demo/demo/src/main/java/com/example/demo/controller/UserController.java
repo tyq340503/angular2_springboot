f17d8bd6d2abde99c45e507da3b694f2d2340a2e
tyq <1135409448@qq.com>
2019-01-12 18:52:37 -0500

    https://stackoverflow.com/questions/52190102/spring-boot-annotation-getmapping-can-not-be-resolved-to-a-type

--- demo/demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.repository.UserRepository;
 
